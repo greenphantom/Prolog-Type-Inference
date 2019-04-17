9598640d1ed0011b58024a0ceb507d8a716b01fb
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-04-17 11:47:07 -0400

    Init Commit

---
+++ typeInf.pl
@@ -0,0 +1,111 @@
+/* match functions by unifying with arguments 
+    and infering the result
+*/
+typeExp(Fct, T):-
+    \+ var(Fct), /* make sure Fct is not a variable */ 
+    \+ atom(Fct), /* or an atom */
+    functor(Fct, Fname, _Nargs), /* ensure we have a functor */
+    !, /* if we make it here we do not try anything else */
+    Fct =.. [Fname|Args], /* get list of arguments */
+    append(Args, [T], FType), /* make it loook like a function signature */
+    functionType(Fname, TArgs), /* get type of arguments from definition */
+    typeExpList(FType, TArgs). /* recurisvely match types */
+
+/* propagate types */
+typeExp(T, T).
+
+/* list version to allow function mathine */
+typeExpList([], []).
+typeExpList([Hin|Tin], [Hout|Tout]):-
+    typeExp(Hin, Hout), /* type infer the head */
+    typeExpList(Tin, Tout). /* recurse */
+
+/* TODO: add statements types and their type checking */
+/* global variable definition
+    Example:
+        gvLet(v, T, int) ~ let v = 3;
+ */
+typeStatement(gvLet(Name, T, Code), unit):-
+    atom(Name), /* make sure we have a bound name */
+    typeExp(Code, T), /* infer the type of Code and ensure it is T */
+    bType(T), /* make sure we have an infered type */
+    asserta(gvar(Name, T)). /* add definition to database */
+
+/* Code is simply a list of statements. The type is 
+    the type of the last statement 
+*/
+typeCode([S], T):-typeStatement(S, T).
+typeCode([S, S2|Code], T):-
+    typeStatement(S,_T),
+    typeCode([S2|Code], T).
+
+/* top level function */
+infer(Code, T) :-
+    is_list(Code), /* make sure Code is a list */
+    deleteGVars(), /* delete all global definitions */
+    typeCode(Code, T).
+
+/* Basic types
+    TODO: add more types if needed
+ */
+bType(int).
+bType(float).
+bType(string).
+bType(unit). /* unit type for things that are not expressions */
+/*  functions type.
+    The type is a list, the last element is the return type
+    E.g. add: int->int->int is represented as [int, int, int]
+    and can be called as add(1,2)->3
+ */
+bType([H]):- bType(H).
+bType([H|T]):- bType(H), bType(T).
+
+/*
+    TODO: as you encounter global variable definitions
+    or global functions add their definitions to 
+    the database using:
+        asserta( gvar(Name, Type) )
+    To check the types as you encounter them in the code
+    use:
+        gvar(Name, Type) with the Name bound to the name.
+    Type will be bound to the global type
+    Examples:
+        g
+
+    Call the predicate deleveGVars() to delete all global 
+    variables. Best wy to do this is in your top predicate
+*/
+
+deleteGVars():-retractall(gvar), asserta(gvar(_X,_Y):-false()).
+
+/*  builtin functions
+    Each definition specifies the name and the 
+    type as a function type
+
+    TODO: add more functions
+*/
+
+fType(iplus, [int,int,int]).
+fType(fplus, [float, float, float]).
+fType(fToInt, [float,int]).
+fType(iToFloat, [int,float]).
+fType(print, [_X, unit]). /* simple print */
+
+*/ Find function signature
+   A function is either buld in using fType or
+   added as a user definition with gvar(fct, List)
+*/
+
+% Check the user defined functions first
+functionType(Name, Args):-
+    gvar(Name, Args),
+    is_list(Args). % make sure we have a function not a simple variable
+
+% Check first built in functions
+functionType(Name, Args) :-
+    fType(Name, Args), !. % make deterministic
+
+% This gets wiped out but we have it here to make the linter happy
+%gvar(_, _) :- false().
+
+:- dynamic(gvar/2).
---
+++ typeInf.plt
@@ -0,0 +1,44 @@
+:- begin_tests(typeInf).
+:- include(typeInf). 
+
+/* Note: when writing tests keep in mind that 
+    the use of of global variable and function definitions
+    define facts for gvar() predicate. Either test
+    directy infer() predicate or call
+    delegeGVars() predicate to clean up gvar().
+*/
+
+% tests for typeExp
+test(typeExp_iplus) :- 
+    typeExp(iplus(int,int), int).
+
+% this test should fail
+test(typeExp_iplus_F, [fail]) :-
+    typeExp(iplus(int, int), float).
+
+test(typeExp_iplus_T, [true(T == int)]) :-
+    typeExp(iplus(int, int), T).
+
+% NOTE: use nondet as option to test if the test is nondeterministic
+
+% test for statement with state cleaning
+test(typeStatement_gvar, [nondet, true(T == int)]) :- % should succeed with T=int
+    deleteGVars(), /* clean up variables */
+    typeStatement(gvLet(v, T, iplus(X, Y)), unit),
+    assertion(X == int), assertion( Y == int), % make sure the types are int
+    gvar(v, int). % make sure the global variable is defined
+
+% same test as above but with infer 
+test(infer_gvar, [nondet]) :-
+    infer([gvLet(v, T, iplus(X, Y))], unit),
+    assertion(T==int), assertion(X==int), assertion(Y=int),
+    gvar(v,int).
+
+% test custom function with mocked definition
+test(mockedFct, [nondet]) :-
+    deleteGVars(), % clean up variables since we cannot use infer
+    asserta(gvar(my_fct, [int, float])), % add my_fct(int)-> float to the gloval variables
+    typeExp(my_fct(X), T), % infer type of expression using or function
+    assertion(X==int), assertion(T==float). % make sure the types infered are correct
+
+:-end_tests(typeInf).
